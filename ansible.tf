f9e5f865e524ee5aa7acb66a165caf2a63d1d0dd
uthmanakz <uthmanakinwumi1@gmail.com>
2024-12-29 15:50:08 +0000

    testing ansible-playbook in ansible machine

--- ansible.tf
+++ ansible.tf
@@ -2,7 +2,7 @@ resource "aws_instance" "ansible" {
 
   ami           = "ami-019374baf467d6601"
   instance_type = "t2.micro"
-  key_name      = "uthmanakz"
+  key_name      = "simbababy"
 
   tags = {
     Name = "Ansible"
--- main.tf
+++ main.tf
@@ -7,7 +7,7 @@ resource "aws_instance" "webnodes" {
   count         = "2"
   ami           = "ami-019374baf467d6601"
   instance_type = "t2.micro"
-  key_name      = "uthmanakz"
+  key_name      = "simbababy"
 
   tags = {
     Nmae = "web-server-${count.index + 1}"
