9e1d962fb21d9c07587e579d5cac27dadc1485d7
uthmanakz <uthmanakinwumi1@gmail.com>
2024-12-29 14:16:54 +0000

    testing terraform workflow

---
+++ ansible.tf
@@ -0,0 +1,15 @@
+resource "aws_instance" "ansible" {
+
+  ami           = "ami-019374baf467d6601"
+  instance_type = "t2.micro"
+  key_name      = "uthmanakz"
+
+  tags = {
+    Name = "Ansible"
+  }
+
+}
+
+output "ansible_public_ip" {
+value = aws_instance.ansible.public_ip
+}
---
+++ main.tf
@@ -0,0 +1,30 @@
+provider "aws" {
+  region = "eu-west-2"
+}
+
+
+resource "aws_instance" "webnodes" {
+  count         = "2"
+  ami           = "ami-019374baf467d6601"
+  instance_type = "t2.micro"
+  key_name      = "uthmanakz"
+
+  tags = {
+    Nmae = "web-server-${count.index + 1}"
+  }
+
+}
+
+resource "null_resource" "ansible-inventory" {
+  depends_on = [aws_instance.webnodes]
+
+  provisioner "local-exec" {
+    command = <<EOF
+        echo "[web-server]" > ansible-CM/inventory.ini
+        echo "${join("\n", aws_instance.webnodes.*.public_ip)}" >> ansible-CM/inventory.ini
+        EOF
+  }
+
+
+}
+
